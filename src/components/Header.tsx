1e93441744d1017d976b857a03bd918276708361
madhukar47b <madhukar.chouhan@47billion.com>
2024-04-29 19:09:28 +0530

    fix: added routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,25 @@
 "use client";
 
 const Header = () => {
-  const menuList: string[] = [
-    "Investment opportunity",
-    "How it works",
-    "About us",
+  const menuList: { name: string; route: string }[] = [
+    { name: "Investment opportunity", route: "Home" },
+    { name: "How it works", route: "Projects" },
+    { name: "About us", route: "Team" },
   ];
   return (
     <nav className="py-2 px-4 bg-[#67bdce]">
       <div className="flex justify-between  md:container md:mx-auto">
         <div className="font-bold py-3 text-white">NEXT INVEST</div>
         <div className="p-3">
-          {menuList.map((item, index) => {
-            return (
-              <a key={index} href={item} className="font-medium p-3 text-white">
-                {item}
-              </a>
-            );
-          })}
+          {menuList.map((item, index) => (
+            <a
+              key={index}
+              href={`/${item.route}`}
+              className="font-medium p-3 text-white"
+            >
+              {item.name}
+            </a>
+          ))}
         </div>
       </div>
     </nav>
